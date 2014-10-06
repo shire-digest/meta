fdde032cf6952c76e3fcb67883382a657cb4278b
hbc <bcxxxxxx@gmail.com>
2014-10-06 15:39:06 +0800

    Init.

---
+++ README.md
@@ -0,0 +1,9 @@
+# meta
+
+;)
+
+## License
+
+Copyright © 2014 hbc
+
+Distributed under the [SMPPL](https://github.com/xhacker/SMPPL/blob/master/SMPPL-Freeware.md) License.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject meta "0.0.1-SNAPSHOT"
+  :description ";)"
+  :license {:name "SMPPL"
+            :url "https://github.com/xhacker/SMPPL/blob/master/SMPPL-Freeware.md"}
+
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  
+  :source-paths ["src"])
